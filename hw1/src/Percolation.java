2ed23bff310c634a48bfa2a402c3b738bad32a45
Rulovescode <rujia721@gmail.com>
2017-08-24 12:18:31 -0700

    Completed hw1 for 100/100

--- hw1/src/Percolation.java
+++ hw1/src/Percolation.java
@@ -2,6 +2,125 @@
  * Created by jia on 7/28/17.
  */
 
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private WeightedQuickUnionUF backWash;
+    private boolean[] openState;
+    private boolean[] fullState;
+    private boolean percolateState;
+
+
+    private int N;
+    private int siteCount;
+    private int openSiteCount = 0;
+
+
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("N should be a positive number!");
+
+        N = n;
+        siteCount = n * n;
+        openState = new boolean[siteCount + 2];
+        fullState = new boolean[N];
+        backWash = new WeightedQuickUnionUF(siteCount+2);
+
+        for (int i = 1; i <= siteCount; i++) {
+            if (i < N) {
+                fullState[i] = false;
+            }
+            openState[i] = false;
+        }
+        openState[0] = true;
+        openState[siteCount + 1] = true;
+    }
+
+    // open site (row, col) if it is not open already
+    // First, it should validate the indices of the site that it receives.
+    // Second, it should somehow mark the site as open.
+    // Third, it should perform some sequence of WeightedQuickUnionUF operation
+    // that links the site in question to its open neighbors.
+    // The constructor and instance variables should facilitate the open() method's ability to do its job.
+    public    void open(int row, int col) {
+        validate(row, col);
+        int position = xyTo1D(row, col);
+        if (!isOpen(row, col)) {
+            openState[position] = true;
+            openSiteCount++;
+
+            if (row == 1) {
+                backWash.union(0, position);
+            }
+
+            if (row > 1 && isOpen(row - 1, col)) {
+                backWash.union(xyTo1D(row - 1, col), position);
+            }
+            if (row < N && isOpen(row + 1, col)) {
+                backWash.union(xyTo1D(row + 1, col), position);
+            }
+            if (col > 1 && isOpen(row, col - 1)) {
+                backWash.union(position - 1, position);
+            }
+            if (col < N && isOpen(row, col+1)) {
+                backWash.union(position + 1, position);
+            }
+        }
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return openState[xyTo1D(row, col)];
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        if (isOpen(row, col)){
+            if (row == N){
+                fullState[col - 1] = backWash.connected(xyTo1D(row, col), 0);
+                if (fullState[col - 1]) {
+                    percolateState = true;
+                }
+                return fullState[col -1];
+            } else return backWash.connected(xyTo1D(row, col), 0);
+        }
+        return false;
+    }
+
+    // number of open sites
+    public     int numberOfOpenSites() {
+        return openSiteCount;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return percolateState;
+    }
+
+    private int xyTo1D(int row, int col) {
+        return (row - 1) * N + col;
+    }
+
+    private void validate(int row, int col) {
+        if (row <= 0 || row > N) {
+            throw new IllegalArgumentException("row " + row +  " is not between 0 and " + N);
+        }
+        if (col <= 0 || col > N) {
+            throw new IllegalArgumentException("column" + col + " is not between 0 and " + N);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -115,3 +234,4 @@ public class Percolation {
     }
 }
 
+*/
